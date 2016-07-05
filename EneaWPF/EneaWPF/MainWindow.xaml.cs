101c3f729a79847e504d010c7630c79f8eff6873
Mateusz Klosinski <matix003@gmail.com>
2016-07-05 20:36:44 +0200

    Dodanie oczekiwania na pobranie danych przed pokazaniem listy.

--- EneaWPF/EneaWPF/MainWindow.xaml.cs
+++ EneaWPF/EneaWPF/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace EneaWPF
 {
@@ -20,16 +21,33 @@ namespace EneaWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-
+        DispatcherTimer timer = new DispatcherTimer();
         AppManager manager;
         public MainWindow()
         {
             InitializeComponent();
             manager = new AppManager();
             grid.DataContext = manager;
-
+            timer.Interval = TimeSpan.FromSeconds(0.7);
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (todayListBox.Items.Count == 0 || tommorowListBox.Items.Count == 0 || elseListBox.Items.Count == 0)
+            {
+                downloadingTextBlock.Text += ".";
+                if (downloadingTextBlock.Text.Length > 20)
+                    downloadingTextBlock.Text = "Pobieranie danych.";
+            }
+            else
+            {
+                downloadingTextBlock.Visibility = Visibility.Collapsed;
+                mainStackPanel.Visibility = Visibility.Visible;
+                timer.Stop();
+            }
+        }
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
