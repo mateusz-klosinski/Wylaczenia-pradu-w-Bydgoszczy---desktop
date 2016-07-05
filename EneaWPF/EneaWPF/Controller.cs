c96c606642272dea0c91d1ba0a415c76e98c6992
Mateusz Klosinski <matix003@gmail.com>
2016-07-05 13:21:17 +0200

    Operacje na stringach, dodanie regexow.

--- EneaWPF/EneaWPF/Controller.cs
+++ EneaWPF/EneaWPF/Controller.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace EneaWPF
 {
@@ -39,7 +40,15 @@ namespace EneaWPF
         {
             WebClient client = new WebClient();
             Uri uri = new Uri("http://www.wylaczenia-eneaoperator.pl/index.php?rejon=17");
-            downloadedData = await client.DownloadStringTaskAsync(uri);
+            try
+            {
+                downloadedData = await client.DownloadStringTaskAsync(uri);
+            }
+            catch (WebException)
+            {
+                MessageBox.Show("Nie można pobrać danych ze strony Enea", "Błąd sieci");
+            }
+
         }
 
 
@@ -51,8 +60,9 @@ namespace EneaWPF
             downloadedData = null;
             downloadedDataList.Clear();
             await downloadString();
-            await createFile();
+            await createFileFromString();
             await createListFromFile();
+            removeUnnecesaryInformationFromList();
             test = createListToShow();
             OnPropertyChanged("DataList");
         }
@@ -61,11 +71,11 @@ namespace EneaWPF
 
 
 
-        private async Task createFile()
+        private async Task createFileFromString()
         {
             await downloadString();
 
-            using (StreamWriter writer = new StreamWriter(new FileStream(@"c:\enea\enea.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite), Encoding.Default))
+            using (StreamWriter writer = new StreamWriter(new FileStream(@"enea.html", FileMode.OpenOrCreate, FileAccess.ReadWrite), Encoding.Default))
             {
                 await writer.WriteLineAsync(downloadedData);
             }
@@ -77,7 +87,7 @@ namespace EneaWPF
 
         private async Task createListFromFile()
         {
-            using (StreamReader reader = new StreamReader(new FileStream(@"c:\enea\enea.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite)))
+            using (StreamReader reader = new StreamReader(new FileStream(@"enea.html", FileMode.OpenOrCreate, FileAccess.ReadWrite)))
             {
                 while (!reader.EndOfStream)
                 {
@@ -85,6 +95,32 @@ namespace EneaWPF
                 }
             }
 
+        }
+
+
+
+        private void makeMagicWithList()
+        {
+            List<string> copiedList = downloadedDataList;
+            downloadedDataList.Clear();
+            foreach(string line in copiedList)
+            {
+                downloadedDataList.Add(TagRemoveHelper.CutTagsFromHtml(line));
+            }
+        }
+
+
+        private void removeUnnecesaryInformationFromList()
+        {
+            string importantLine = string.Empty;
+            foreach (string line in downloadedDataList)
+            {
+                if (line.Contains("Wyłączenia w rejonie dystrybucji"))
+                {
+                    importantLine = line;
+                }
+            }
+            downloadedDataList.RemoveRange(0, (downloadedDataList.IndexOf(importantLine) + 1));
         }
 
 
---
+++ EneaWPF/EneaWPF/Disconnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EneaWPF
+{
+    class Disconnection
+    {
+        public string Area { get; private set; }
+        public string Data { get; private set; }
+        public string Details { get; private set; }
+
+        public Disconnection(string area, string data, string details)
+        {
+            Area = area;
+            Data = data;
+            Details = details;
+        }
+
+    }
+}
